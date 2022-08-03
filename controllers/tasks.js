47a3ab73cea815c73a84aee763b039b163e9017a
livos <lvivancos2nd-dev@yahoo.fr>
2022-08-03 18:33:48 +0700

    api route not found

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,6 +4,7 @@ const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
+    //.json({ status: "success", data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
